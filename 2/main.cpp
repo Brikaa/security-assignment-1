0aa5388786ab63b59b0e8b489437c9e38bab49e8
Omar Brikaa <brikaaomar@gmail.com>
2024-04-08 15:23:10 +0200

    Initial encrypt, decrypt

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,6 +1,8 @@
 #include <random>
 #include <iostream>
 #include <utility>
+#include <array>
+#include <cmath>
 typedef unsigned long long ull;
 typedef unsigned int ui;
 
@@ -67,8 +69,58 @@ std::pair<PublicKey, PrivateKey> generate_public_private_key_pair()
     return {{e, n}, {d, p, q}};
 }
 
+ull multiply_mod(ull a, ull b, ull mod)
+{
+    // 8*3 = 2*4*b = 2*2*2*b
+    // 9*3 = b + 2*4*b = b + 2*2*2*b
+    ull res = 0;
+    while (a != 1)
+    {
+        if (a % 2 == 1)
+        {
+            res = (res + b) % mod;
+            --a;
+        }
+        b = (b + b) % mod;
+        a /= 2;
+    }
+    return (res + b) % mod;
+}
+
+ull power_mod(ull base, ull exponent, ull mod)
+{
+    // 2 ** 8 = ((2 ** 2) ** 2) ** 2
+    // 2 ** 9 = 2 * ((2 ** 2) ** 2) ** 2
+    ull res = 1;
+    while (exponent != 1)
+    {
+        if (exponent % 2 == 1)
+        {
+            res = multiply_mod(res, base, mod);
+            --exponent;
+        }
+        base = multiply_mod(base, base, mod);
+        exponent /= 2;
+    }
+    return multiply_mod(res, base, mod);
+}
+
+ull encrypt(ull m, ull e, ull n)
+{
+    return power_mod(m, e, n);
+}
+
+ull decrypt(ull c, ull d, ull p, ull q)
+{
+    return power_mod(c, d, p * q);
+}
+
 int main()
 {
     std::srand(time(nullptr));
+    auto [pub, _] = generate_public_private_key_pair();
+    ull m = 255;
+    std::cout << m << ' ' << pub.e << ' ' << pub.n << '\n';
+    std::cout << encrypt(255, pub.e, pub.n) << '\n';
     return 0;
 }
